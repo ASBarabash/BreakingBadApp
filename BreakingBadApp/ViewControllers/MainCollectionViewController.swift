8f82939d600c8c1c76f704e54d4037ac4b103dfc
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-04-16 14:53:55 +0300

    Add Alamofire

---
+++ BreakingBadApp/ViewControllers/MainCollectionViewController.swift
@@ -0,0 +1,113 @@
+//
+//  MainCollectionViewController.swift
+//  BreakingBadApp
+//
+//  Created by Alexandr Barabash on 16.04.2022.
+//
+
+import UIKit
+
+//Подписываем под протокол CaseIterable, что позволит нам обратившись к типу данных UserAction вызвать геттер allCases
+enum UserAction: String, CaseIterable {
+    case showBreakinBad = "Breaking Bad"
+    case showRandomImage = "Random Image"
+}
+
+
+class MainCollectionViewController: UICollectionViewController {
+    
+    let userActions = UserAction.allCases
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 2
+//    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        userActions.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UserActionCell else { return UICollectionViewCell() }
+        
+        cell.userActionLabel.text = userActions[indexPath.item].rawValue
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let userAction = userActions[indexPath.item]
+        switch userAction {
+        case .showBreakinBad: performSegue(withIdentifier: "showBreakingBad", sender: nil)
+        case .showRandomImage: performSegue(withIdentifier: "showRandomImage", sender: nil)
+        }
+    }
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension MainCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+        
+    }
+}
+
+// MARK: - Networking
+extension MainCollectionViewController {
+    
+    func fetchBreakingBad() {
+        
+    }
+    
+    func fetchRandomImage() {
+        
+    }
+    
+}
---
+++ BreakingBadApp/ViewControllers/UserActionCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserActionCell.swift
+//  BreakingBadApp
+//
+//  Created by Alexandr Barabash on 16.04.2022.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    @IBOutlet var userActionLabel: UILabel!
+}
