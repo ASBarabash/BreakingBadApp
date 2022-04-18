05950f58e993b174480b9dd84ec5915efddcbff6
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-04-18 13:14:34 +0300

    Add Random Image VC

---
+++ BreakingBadApp/ViewControllers/RandomImageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RandomImageViewController.swift
+//  BreakingBadApp
+//
+//  Created by Alexandr Barabash on 18.04.2022.
+//
+
+import UIKit
+
+class RandomImageViewController: UIViewController {
+
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func getNextImage() {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
